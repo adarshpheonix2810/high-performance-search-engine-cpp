65cd54a4f99245c1e5629cabed591e94aa077bb5
adarshpheonix2810 <adarshsingh786000@gmail.com>
2025-12-26 05:23:17 +0530

    fixed memory bug and added error checks

--- src/Document_store.cpp
+++ src/Document_store.cpp
@@ -36,15 +36,35 @@ int read_sizes(int *linecounter,int *maxlength, char *file_name){
     return 1;
 }
 void split(char* temp,int id,TrieNode* trie,Mymap* mymap){
+    char* token;
+    token = strtok(temp, " \t");
+    int i=0;
+    while(token != NULL){
+        
+        i++;
+        trie->insert(token, id);
+        token = strtok(NULL, " \t");
+    }
+    mymap->setlength(id,i);
+
 }
 int read_input(Mymap* mymap,TrieNode *trie, char* file_name){
     FILE *file = fopen(file_name, "r");
+    if(file == NULL){
+        cout << "Error opening file: " << file_name << endl;
+        return -1;
+    }
     char *line = NULL;
     size_t buffersize = 0;
-    int current_length;
     char *temp = (char*)malloc(mymap->get_buffersize()*sizeof(char));
     for(int i=0;i<mymap->get_size();i++){
-        getline(&line, &buffersize, file);
+        if(getline(&line, &buffersize, file) == -1){
+            cout << "Error reading line " << i << endl;
+            free(line);
+            fclose(file);
+            free(temp);
+            return -1;
+        }
         if (mymap->insert(line, i) == -1) {
             cout << "Error inserting line " << endl;
             free(line);
