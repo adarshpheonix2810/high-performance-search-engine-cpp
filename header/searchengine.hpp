b4ee7b86f82458b19cd1fda5dfcc08a9a973fcd5
adarshpheonix2810 <adarshsingh786000@gmail.com>
2025-12-26 05:23:45 +0530

    cleaned up include paths

--- header/searchengine.hpp
+++ header/searchengine.hpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
-#include "./Document_store.hpp"
-#include "./Map.hpp"
+#include "Document_store.hpp"
+#include "Map.hpp"
 #include "Trie.hpp"
