9f6d8bcec643bcbda5e0898b754ace6897dde43e
adarshpheonix2810 <adarshsingh786000@gmail.com>
2025-12-28 17:58:39 +0530

    fixed memory bug and exit logic

--- src/Searchengine.cpp
+++ src/Searchengine.cpp
@@ -1,6 +1,35 @@
 #include "Searchengine.hpp"
 
 using namespace std;
+
+int inputmanager(char* input, TrieNode* trie, Mymap* mymap, int k){
+    char* token=strtok(input, " \t\n");
+    
+    if(token == NULL){
+        return 0;  // Empty input, continue
+    }
+    
+    if(!strcmp(token,"/search")){
+        search(token,trie,mymap,k);
+        return 1;
+    }
+    else if(!strcmp(token,"/df")){
+        df(trie);
+        return 1;
+    }
+    else if(!strcmp(token,"/tf")){
+        tf(token,trie);
+        return 1;
+    }
+    else if(!strcmp(token,"/exit")){
+        return 2;  // Signal to exit
+    }
+    else{
+        cout<<"Unknown command: "<<token<<endl;
+        cout<<"Available commands: /search, /df, /tf, /exit"<<endl;
+        return 0;  // Continue, not exit
+    }
+}
 // read document/books/searchengine.md for more information
 int main(int argc, char** argv) {
     if (argc != 5 ||
@@ -33,7 +62,24 @@ int main(int argc, char** argv) {
         return -1;
     }
     cout<<"File read successfully. Lines: " << linecounter << ", Max Length: " << maxlength << endl;
+    char* input=NULL;
+    size_t input_length=0;
+    while(1){
+        cout << "Enter query (or type '/exit' to quit): ";
+        if(getline(&input, &input_length, stdin) == -1){
+            // EOF or error
+            break;
+        }
+        
+        int ret=inputmanager(input,trie,mymap,k);
+        if(ret==2){
+            cout<<"Exiting program..."<<endl;
+            break;
+        }
+        // ret == 0 or 1: continue
+    }
     
+    free(input);
     delete (mymap);
     delete (trie);
     return 0;
