c29c8028e12ff06597eb83272ec6d20b301fce6b
adarshpheonix2810 <adarshsingh786000@gmail.com>
2025-12-28 17:59:22 +0530

    added search header file

---
+++ header/Search.hpp
@@ -0,0 +1,17 @@
+#ifndef SEARCH_HPP
+#define SEARCH_HPP
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include "Map.hpp"
+#include "Trie.hpp"
+
+using namespace std;
+
+// Function declarations
+void search(char* token, TrieNode *trie, Mymap *map, int k);
+void df(TrieNode* trie);
+int tf(char* token, TrieNode* trie);
+
+#endif
